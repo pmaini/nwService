aa4f0c1c0f90132f9db8911488b50a9d6df20004
pmaini <parikshitm@iiitd.ac.in>
2014-05-12 15:15:43 +0530

    range neighbourhood
    neighbours are now found from cell or area boundaries depending upon
    single or multi cell node

--- get_neighbours_in_range.m
+++ get_neighbours_in_range.m
@@ -10,24 +10,30 @@ function neighbours = get_neighbours_in_range(cell_index,range)
 %%a direction is atleast 1 unit away and the maximum that this difference
 %%of reference can give is (sqrt(2)/2) which is arnd 0.712...so it is okay!
 global gridpoints_x gridpoints_y;
-xc = gridpoints_x(cell_index(:));
-yc = gridpoints_y(cell_index(:));
-dx = max(xc) - min(xc);
-dy = max(yc) - min(yc);
+xc_0 = gridpoints_x(cell_index(:));
+xc_1 = xc_0 - 0.5;
+xc_2 = xc_0 + 0.5;
+xc = [xc_1 xc_2 xc_1 xc_2];
+yc_0 = gridpoints_y(cell_index(:));
+yc_1 = yc_0 - 0.5;
+yc_2 = yc_0 + 0.5;
+yc = [yc_1 yc_2 yc_2 yc_1];
+% dx = max(xc) - min(xc);
+% dy = max(yc) - min(yc);
 neighbours = [];
 
 z = ceil(range);
 
-xl = min(xc - z);
-yl = min(yc - z);
-xh = max(xc + z);
-yh = max(yc + z);
+xl = min(xc_0 - z);
+yl = min(yc_0 - z);
+xh = max(xc_0 + z);
+yh = max(yc_0 + z);
 
 x = xl:1:xh;
 y = yl:1:yh;
 
-xpoly = [min(xc),min(xc),min(xc)+dx,min(xc)+dx,min(xc)];
-ypoly = [min(yc),min(yc)+dy,min(yc)+dy,min(yc),min(yc)];
+% xpoly = [min(xc),min(xc),min(xc)+dx,min(xc)+dx,min(xc)];
+% ypoly = [min(yc),min(yc)+dy,min(yc)+dy,min(yc),min(yc)];
 
 indices = [];
 for i = 1:length(y)
@@ -44,7 +50,10 @@ for i = 1:length(y)
         end
     end
 end
-% 
+
+% This part commented out so this function always returns the max_neighbour
+% number of neighbours. 0 if its invalid
+
 % neighbours(neighbours==0) = [];
 % neighbours = unique(neighbours);
 % neighbours = sort(neighbours);
