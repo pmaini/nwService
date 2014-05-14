d5d971aa4c6d7ef28703e724bd941d1ee496c88e
pmaini <parikshitm@iiitd.ac.in>
2014-05-14 20:00:29 +0530

    integrated for current strategy

--- refresh_agent_plot.m
+++ refresh_agent_plot.m
@@ -13,23 +13,26 @@ end
 for i = 1:numAgent
     
     if(agent_node(i).task_assigned > 0)
-        if mod(agent_node(i).current_task,2) == 0
-            agent_node(i).plot(1) = plot(agent_node(i).xc,agent_node(i).yc,'mo','MarkerFaceColor','magenta','MarkerSize',10);
-        else
+%         if mod(agent_node(i).current_task,2) == 0
             agent_node(i).plot(1) = plot(agent_node(i).xc,agent_node(i).yc,'mo','MarkerFaceColor','blue','MarkerSize',10);
-        end
-        
-    elseif (agent_node(i).free == 0)
+            agent_node(i).plot(2) = text(agent_node(i).xc - 0.3,agent_node(i).yc,[num2str(i) '-T' num2str(find(agent_node(i).role_vector==1))]);
+%         else
+%             agent_node(i).plot(1) = plot(agent_node(i).xc,agent_node(i).yc,'mo','MarkerFaceColor','blue','MarkerSize',10);
+%         end
+%         
+%     elseif (agent_node(i).free == 0)
+%         agent_node(i).plot(1) = plot(agent_node(i).xc,agent_node(i).yc,'mo','MarkerFaceColor','green','MarkerSize',10);
+%         
+    elseif (agent_node(i).in_relay > 0)
         agent_node(i).plot(1) = plot(agent_node(i).xc,agent_node(i).yc,'mo','MarkerFaceColor','green','MarkerSize',10);
-        
-    elseif (agent_node(i).in_relay == 1)
-        agent_node(i).plot(1) = plot(agent_node(i).xc,agent_node(i).yc,'mo','MarkerFaceColor','cyan','MarkerSize',10);
-        
+        agent_node(i).plot(2) = text(agent_node(i).xc - 0.3,agent_node(i).yc,num2str(i));
     else
         agent_node(i).plot(1) = plot(agent_node(i).xc,agent_node(i).yc,'mo','MarkerFaceColor','yellow','MarkerSize',10);
+        agent_node(i).plot(2) = text(agent_node(i).xc - 0.3,agent_node(i).yc,num2str(i));
+        
     end
-    
-    agent_node(i).plot(2) = text(agent_node(i).xc - 0.3,agent_node(i).yc,num2str(i));
     set(agent_node(i).plot(2),'FontSize',8,'FontName','FixedWidth');
+    
+
     
 end
