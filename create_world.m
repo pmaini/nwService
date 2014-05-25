1d9e2e06c259e39f52303cdfba8a845845d92192
pmaini <parikshitm@iiitd.ac.in>
2014-05-25 22:58:01 +0530

    integrated new los matrix computation
    integration after removing agent dependency.
    commit to main branch

--- create_world.m
+++ create_world.m
@@ -5,9 +5,9 @@ function [base, obstacle, agent_node, task_node] = create_world(filename)
 %%argument.
 
 global grid_type numRows numColumns xmin ymin env_graph numCells xmax ymax;
-global numAgent numTask numObstacle sim_cont comm_network aC_range bC_range;
+global numAgent numTask numObstacle sim_cont comm_network aC_range bC_range aT_range;
 global task_counter max_tasks current_tasks time Total_obstacle_space d_max;
-global size_x size_y size_g gridpoints_x gridpoints_y covered_fraction;
+global size_x size_y size_g gridpoints_x gridpoints_y covered_fraction aS_range;
 global gridlocation gridCells s_time index_for_UD losMat cNeighMat maxTargets;
 
 %0:custom, 1:random
@@ -22,15 +22,15 @@ if nargin == 1
 
     % Add Obstacles
     obstacle = generate_obstacle(base,filename);
+    
+    % Los Data
+    [losMat cNeighMat] = getLosMatrix(base,obstacle,filename);
 
     % Define agents
     agent_node = define_agents(base, obstacle, graph, filename);%, obstacle);
 
     % Task Node definition
     task_node = define_tasks(base, obstacle, filename);
-    
-    % Los Data
-    [losMat cNeighMat] = getLosMatrix(base,obstacle,agent_node, filename);
     
 %     save(['a' num2str(numAgent) 't' num2str(numTask) 'o' num2str(numObstacle)]);
 
@@ -49,14 +49,14 @@ elseif nargin == 0
 
     % Add Obstacles
     obstacle = generate_obstacle(base);
-
+        
+    [losMat cNeighMat] = getLosMatrix(base,obstacle);
+    
     % Define agents
     agent_node = define_agents(base, obstacle, graph);%, obstacle);
 
     % Task Node definition
     task_node = define_tasks(base, obstacle);
-    
-    [losMat cNeighMat] = getLosMatrix(base,obstacle,agent_node);
     
     save(['a' num2str(numAgent) 't' num2str(numTask) 'o' num2str(numObstacle)]);
     
