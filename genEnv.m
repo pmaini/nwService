1d3fe106f217330846e4c3348f25986a935f2bbc
pmaini <parikshitm@iiitd.ac.in>
2014-05-25 23:07:24 +0530

    generate the environment
    make graph and add base

---
+++ genEnv.m
@@ -0,0 +1,17 @@
+function genEnv()
+
+global grid_type numRows numColumns xmin ymin env_graph numCells xmax ymax;
+global numAgent numTask numObstacle sim_cont comm_network aC_range bC_range aT_range;
+global task_counter max_tasks current_tasks time Total_obstacle_space d_max;
+global size_x size_y size_g gridpoints_x gridpoints_y covered_fraction aS_range;
+global gridlocation gridCells s_time index_for_UD losMat cNeighMat maxTargets;
+
+    initEnv;
+
+    env_graph = make_graph();
+    % view(biograph(env_graph,[],'ShowWeights','on','ShowArrows','off'));
+
+    % Define base station
+    base = define_baseStation();
+
+    save('20Env');
