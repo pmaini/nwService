e164abf04bf939db51950e6c90646a6ac5bd23be
pmaini <parikshitm@iiitd.ac.in>
2014-05-29 08:21:47 +0530

    Los computation upgraded
    now LoS computation done for all ranges. Support for same added to
    getLoSMatrix.

--- getLosMatrix.m
+++ getLosMatrix.m
@@ -1,35 +1,45 @@
 function [rval1 rval2] = getLosMatrix(base, obstacle, filename)
 
-global numCells gridpoints_x gridpoints_y aC_range;
+global numCells gridpoints_x gridpoints_y aC_range gridCells aS_range aT_range;
 
 if nargin == 3
     load(filename);
     rval1 = losMat;
-    rval2 = cNeighMat;
+    rval2 = NeighMat;
     return;
 end
 
-range = aC_range;
-maxnNum = length(get_neighbours_in_range(base.c_neighbours{6,1},range));
-neighbourMatrix = zeros(numCells,maxnNum);
-losMatrix = zeros(numCells,maxnNum);
+range = [aC_range aS_range aT_range];
+maxnNum = length(get_neighbours_in_range(base.indices(1),max(range)));
+neighbourMatrix = zeros(numCells,maxnNum,3);
+losMatrix = zeros(numCells,maxnNum,3);
+
+
 
 for i = 1:numCells
-    neigh = get_neighbours_in_range(i,range);
+    neigh = gridCells(i).c_neighbours;% get_neighbours_in_range(i,range);
     neigh(ismember(neigh,base.indices)) = 0;
     neigh(ismember(neigh,[obstacle(:).index])) = 0;
-    neighbourMatrix(i,:) = neigh;
+    neighC = neigh;
+    neighS = gridCells(i).s_neighbours(ismember(gridCells(i).s_neighbours,neigh));
+    neighT = gridCells(i).t_neighbours(ismember(gridCells(i).t_neighbours,neigh));
+
+    neighbourMatrix(i,:,1) = neighC;
+    neighbourMatrix(i,1:length(neighS),2) = neighS;
+    neighbourMatrix(i,1:length(neighT),3) = neighT;
 %     neighbourMatrix(i,:) = neigh;%setdiff(get_neighbours_in_range(i,range),[obstacle(:).index]);
 end
 
 for i = 1:numCells
-    for j = 1:maxnNum
-        nIndex = neighbourMatrix(i,j);
-        if nIndex == 0
-            losMatrix(i,j) = 0;
-            continue;
+    for r = 1:3
+        for j = 1:length(neighbourMatrix(i,:,r))
+            nIndex = neighbourMatrix(i,j,r);
+            if nIndex == 0
+                losMatrix(i,j,r) = 0;
+                continue;
+            end
+            losMatrix(i,j,r) = isLosClear(gridpoints_x(i),gridpoints_y(i),gridpoints_x(nIndex),gridpoints_y(nIndex),base,obstacle,1);
         end
-        losMatrix(i,j) = isLosClear(gridpoints_x(i),gridpoints_y(i),gridpoints_x(nIndex),gridpoints_y(nIndex),base,obstacle,1);
     end
 end
 
