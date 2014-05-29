fa27533a6070f6565829c4b05969ee8d6947c0af
pmaini <parikshitm@iiitd.ac.in>
2014-05-29 07:53:52 +0530

    LoS check for base brought out
    From sense_base_neighborhood where los checks were done in every
    iteration, brought out to build_neighbourhood for one time computation.

--- build_neighbourhood.m
+++ build_neighbourhood.m
@@ -3,9 +3,17 @@ function base = build_neighbourhood(base,obstacle)
 %This includes boundary validation and obstacle detection. Agent level
 %sensing would still happen dynamically
 
+%Not only obstacled cells also cells not in LOS are marked null
+xpoly = [base.xc base.xc+base.size(1)-1 base.xc+base.size(1)-1 base.xc base.xc];
+ypoly = [base.yc base.yc base.yc+base.size(2)-1 base.yc+base.size(2)-1 base.yc];
 
-for j = 1:base.max_c_neighbour    
-    if ismember(base.c_neighbours{6,j},[obstacle(:).index])
+for j = 1:base.max_c_neighbour
+%         neighIndex = NeighMat(agents(k).index,:,2)==agents(k).s_neighbours{6,j};
+%         if losMat(agents(k).index,neighIndex,2) == 0
+%             continue;
+%         end
+    connected = isConnectionPossible(base.c_neighbours{1,j},base.c_neighbours{2,j},xpoly,ypoly,base,obstacle,base.c_range);
+    if ismember(base.c_neighbours{6,j},[obstacle(:).index]) || (connected == 0)
         base.c_neighbours{3,j} = 'NULL';
         base.c_neighbours{4,j} = 'NULL';
         base.current_c_neighbour_number = base.current_c_neighbour_number - 1;
--- sense_base_neighbourhood.m
+++ sense_base_neighbourhood.m
@@ -11,21 +11,16 @@ global numAgent ;
 base.connected = [];
 
 % Do not initialize the c_neighbours{3,:} and c_neighbours{4,:}. They are
-% being initialized in get_neighbours function
-
-xpoly = [base.xc base.xc+base.size(1)-1 base.xc+base.size(1)-1 base.xc base.xc];
-ypoly = [base.yc base.yc base.yc+base.size(2)-1 base.yc+base.size(2)-1 base.yc];
+% being initialized in get_neighbours function. Obstructed cells and cells
+% not in LOS are marked as NULL
 
 for j = 1:base.max_c_neighbour
     if (strcmp(base.c_neighbours{3,j},'NULL') == 0)
         base.c_neighbours{4,j} = 'EMPTY';
         for i = 1:numAgent
             if (base.c_neighbours{6,j} ==  agents(i).index)                
-                connected = isConnectionPossible(agents(i).xc,agents(i).yc,base.xpoly,base.ypoly,base,obstacle,base.c_range);
-                if (connected == 1)
-                    base.c_neighbours{4,j} = i;
-                    base.connected = [base.connected i];
-                end
+                base.c_neighbours{4,j} = i;
+                base.connected = [base.connected i];
                 break;
             end
         end
