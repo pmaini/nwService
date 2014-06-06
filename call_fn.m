b53090ac81fcf1d1820f9c328c71f369c5c737e7
pmaini <parikshitm@iiitd.ac.in>
2014-06-06 18:03:38 +0530

    testing code

---
+++ call_fn.m
@@ -0,0 +1,9 @@
+function call_fn(i)
+
+global grid_type numRows numColumns xmin ymin env_graph numCells xmax ymax;
+global numAgent numTask numObstacle sim_cont comm_network aC_range bC_range aT_range;
+global task_counter max_tasks current_tasks time Total_obstacle_space d_max;
+global size_x size_y size_g gridpoints_x gridpoints_y covered_fraction aS_range;
+global gridlocation gridCells s_time index_for_UD losMat NeighMat maxTargets;
+
+clear all;
